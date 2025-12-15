889f3003044742c72f0225dc32a2498e8a3b3f0f
amrz17 <muhammad.amer@gmail.com>
2025-12-15 22:10:17 +0700

    Add DB connection to PostgreSql

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,11 +3,30 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'client', 'dist'),
+    }),
+    // Connect to database
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
     }),
   ],
   controllers: [AppController],
---
+++ readme.txt
@@ -0,0 +1,6 @@
+How to install to spesicif package : 
+npm i --workspace nama_folder @nestjs/serve-static
+
+How to connect PostgreSql to Nest :
+1. npm i pg typeorm @nestjs/typeorm
+2. npm install @nestjs/config
